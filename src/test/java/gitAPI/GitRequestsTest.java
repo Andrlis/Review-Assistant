caa63d63fe34c4930c22616fa46016e49d7d93a9
Andrlis <andrlis0506@gmail.com>
2017-08-22 14:12:07 +0300

    Add Git API methods` tests.

--- src/test/java/gitAPI/GitRequestsTest.java
+++ src/test/java/gitAPI/GitRequestsTest.java
@@ -5,18 +5,39 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class GitRequestsTest extends TestCase {
-    
+
+    private String response;
+    private GitRequests requests;
+
     @Before
     public void setUp() throws Exception {
         super.setUp();
+        requests = new GitRequests();
+        response = null;
     }
 
     @Test
     public void testGetRepoInfo() throws Exception {
+        System.out.println("Test GitRequests.getRepoInfo()");
+        response = requests.getRepoInfo("Andrlis", "TRTPOControlSystem");
+        assertNotNull(response);
+        assertFalse(response.contains("Not Found"));
+        assertTrue(response.contains("Andrlis"));
+        assertTrue(response.contains("TRTPOControlSystem"));
+        System.out.println("Test success.");
     }
 
     @Test
     public void testGetCommitList() throws Exception {
+        System.out.println("Test GitRequests.getCommitList()");
+        response = requests.getCommitList("Andrlis", "TRTPOControlSystem");
+        assertNotNull(response);
+        assertFalse(response.contains("Not Found"));
+        assertTrue(response.contains("Andrlis"));
+        assertTrue(response.contains("TRTPOControlSystem"));
+        assertTrue(response.contains("sha"));
+        assertTrue(response.contains("commit"));
+        System.out.println("Test success.");
     }
 
 }
