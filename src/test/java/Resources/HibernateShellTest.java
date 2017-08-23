9dfbaa278d44216ec062bfe7f17b2f64b70438b5
Andrey Lisovskiy <andrlis0506@gmail.com>
2017-08-24 00:34:33 +0300

    Problems with hibernate tests.

--- src/test/java/Resources/HibernateShellTest.java
+++ src/test/java/Resources/HibernateShellTest.java
@@ -3,8 +3,14 @@ package Resources;
 import Data.Group.Group;
 import Data.Group.GroupsKeeper;
 import Data.Group.SubGroup;
+import Data.Lab.IssuedLab;
+import Data.Lab.Lab;
 import Data.Lab.LabsKeeper;
 import Data.Lecturer;
+import Data.Mark.LabMark;
+import Data.Mark.TestMark;
+import Data.Student;
+import Data.UniversityClass;
 import org.hibernate.Session;
 import org.junit.After;
 import org.junit.Before;
@@ -12,6 +18,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Date;
 
 import static org.junit.Assert.*;
 
@@ -20,31 +27,67 @@ public class HibernateShellTest {
 
     @Before
     public void setUp() throws Exception {
-        Lecturer lecturer = new Lecturer();
-        lecturer.setFullName("Иванов Иван Иваноыич");
-
         group = new Group();
+        Lecturer lecturer = new Lecturer();
+        SubGroup subGroup1 = new SubGroup();
+        UniversityClass universityClass = new UniversityClass();
+        IssuedLab issuedLab = new IssuedLab();
+        Lab lab = new Lab();
+        Student student = new Student();
+        TestMark testMark = new TestMark();
+        LabMark labMark = new LabMark();
+        Data.Test.Test test = new Data.Test.Test();
+
+        lecturer.setFullName("Иванов Иван Иваноыич");
+        lecturer.setSubGroupList(Arrays.asList(subGroup1));
+
         group.setNumberOfGroup("550503");
         group.setScheduleApiGroupNumber("550503");
         group.setAmountOfTest(0);
-        SubGroup subGroup1 = new SubGroup();
+        group.getSubGroupList().add(subGroup1);
+
         subGroup1.setLecturer(lecturer);
         subGroup1.setGroup(group);
-        subGroup1.setUniversityClassesList(null);
+        subGroup1.setUniversityClassesList(Arrays.asList(universityClass));
         subGroup1.setSubGroupNumber("1");
-        subGroup1.setIssuedLabsList(null);
-        subGroup1.setStudentsList(null);
-        SubGroup subGroup2 = new SubGroup();
-        subGroup2.setLecturer(null);
-        subGroup2.setGroup(group);
-        subGroup2.setUniversityClassesList(null);
-        subGroup2.setSubGroupNumber("2");
-        subGroup2.setIssuedLabsList(null);
-        subGroup2.setStudentsList(null);
-        
-        lecturer.setSubGroupList(Arrays.asList(subGroup1,subGroup2));
-        group.getSubGroupList().add(subGroup1);
-        group.getSubGroupList().add(subGroup2);
+        subGroup1.setStudentsList(Arrays.asList(student));
+        subGroup1.setIssuedLabsList(Arrays.asList(issuedLab));
+
+        universityClass.setDate(new Date());
+        universityClass.setSubGroup(subGroup1);
+
+        issuedLab.setCoefficientOfCurrentDeadline(0.8);
+        issuedLab.setCurrentDeadline(universityClass);
+        issuedLab.setDateOfLastRepoCheck(new Date());
+        issuedLab.setLabDescription(lab);
+        issuedLab.setStudentControlList(Arrays.asList(student));
+        issuedLab.setUniversityClassOfIssue(universityClass);
+
+        lab.setKeyWord("lab");
+        lab.setNumberOfLab(1);
+
+        student.seteMail("student@gmail.com");
+        student.setFulName("Петров Петр Петрович");
+        student.setGitRepoName("studentRepo");
+        student.setGitUserName("student");
+        student.setBonusMark(9);
+        student.setLabMarkList(Arrays.asList(labMark));
+        student.setMissedUniversityClassesList(Arrays.asList(universityClass));
+        student.setSubGroup(subGroup1);
+        student.setTestMarkList(Arrays.asList(testMark));
+
+        labMark.setCoefficient(0.2);
+        labMark.setIssuedLab(issuedLab);
+        labMark.setMark(9);
+        labMark.setStudent(student);
+
+        testMark.setMark(8);
+        testMark.setStudent(student);
+        testMark.setTest(test);
+
+        test.setTestDate(new Date());
+        test.setTestNumber(7);
+        test.setTestMarkList(Arrays.asList(testMark));
     }
 
 
@@ -58,43 +101,6 @@ public class HibernateShellTest {
         } finally {
             HibernateShell.delete(group);
         }
-        System.out.println("Test success.");
-    }
-
-    @Test
-    public void testGetGroupKeeper() throws Exception {
-        System.out.println("Test HibernateShell.getGroupKeeper().");
-        GroupsKeeper groupsKeeper = HibernateShell.getGroupKeeper();
-        assertEquals(0, groupsKeeper.getGroupList().size());
-        System.out.println("Test success.");
-    }
-
-    @Test
-    public void getLabsKeeper() throws Exception {
-        System.out.println("Test HibernateShell.getLabsKeeper().");
-        LabsKeeper labsKeeper = HibernateShell.getLabsKeeper();
-        assertEquals(0, labsKeeper.getLabList().size());
-        System.out.println("Test success.");
-    }
-
-    @Test
-    public void testGetNumberOfTests() throws Exception {
-        System.out.println("Test HibernateShell.getNumberOfTests().");
-
-        System.out.println("Test success.");
-    }
-
-    @Test
-    public void teatGetNumberOfLab() throws Exception {
-        System.out.println("Test HibernateShell.getNumberOfLab().");
-
-        System.out.println("Test success.");
-    }
-
-    @Test
-    public void testUpdate() throws Exception {
-        System.out.println("Test HibernateShell.update().");
-
         System.out.println("Test success.");
     }
 }
