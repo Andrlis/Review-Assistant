41c34f7c780d58dc5429c1166158e1116f01d78f
Pavel Kesso <kessopol1998@gmail.com>
2017-12-10 17:58:54 +0300

    added some function from StudentHibernateShell

--- src/main/java/resources/Hibernate/StudentHibernateShell.java
+++ src/main/java/resources/Hibernate/StudentHibernateShell.java
@@ -1,6 +1,15 @@
 package resources.Hibernate;
 
 import data.Student;
+import data.group.Group;
+import data.group.SubGroup;
+import data.lab.IssuedLab;
+import data.mark.LabMark;
+import data.mark.TestMark;
+import data.test.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StudentHibernateShell {
 
@@ -10,6 +19,48 @@ public class StudentHibernateShell {
     }
 
     static public void  NoteStudentPresence(String studentId, String classId){
-        HibernateShell.createQuery("DELETE FROM absentees WHERE id_class = " + classId + " and id_student = " + studentId);
+        HibernateShell.SQLQuery("DELETE FROM absentees WHERE id_class = " + classId + " and id_student = " + studentId);
+    }
+
+    static public void NoteStudentAbsent(String stringId, String classId){
+        HibernateShell.SQLQuery("INSERT INTO absentees (id_class, id_student) VALUES ("+ classId + ", " + stringId + ");");
+    }
+
+    static public boolean saveStudent(String groupNumber, String subGroupNumber, String name, String eMail, String gitURL) {
+        Student student = new Student();
+        student.setFulName(name);
+        student.seteMail(eMail);
+        student.setGitURL(gitURL);
+
+        Group group = HibernateShell.getGroupByGroupNumber(groupNumber);
+        if(group == null)
+            return false;
+
+        SubGroup subGroup = group.getSubGroup(subGroupNumber);
+        if(subGroup == null)
+            return false;
+
+
+        subGroup.addStudent(student);
+
+        for(IssuedLab issuedLab : subGroup.getIssuedLabsList()) {
+            LabMark labMark = new LabMark();
+            labMark.setStudent(student);
+            labMark.setIssuedLab(issuedLab);
+            student.addLabMark(labMark);
+        }
+        List<Test> tests = HibernateShell.getTestKeeper().getTestList();
+        for(Test test : tests){
+            TestMark testMark = new TestMark();
+            testMark.setStudent(student);
+            testMark.setTest(test);
+
+            student.addTestMark(testMark);
+        }
+
+        student.setSubGroup(subGroup);
+
+        HibernateShell.save(student);
+        return true;
     }
 }
