0c4dd2e7f22daad09a29e60d2a5bc13133b41ddc
Andrlis <andrlis0506@gmail.com>
2018-02-05 00:44:56 +0300

    Fix bugs in schedule.

--- src/main/java/timerTasks/ComplexCheckTask.java
+++ src/main/java/timerTasks/ComplexCheckTask.java
@@ -6,7 +6,12 @@ Check repositories and timetable at 00.00
 
 import checker.RepositoryChecker;
 import checker.ScheduleChecker;
-import java.util.Calendar;
+
+import java.lang.reflect.Array;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -14,12 +19,27 @@ import resources.Hibernate.HibernateShell;
 
 public class ComplexCheckTask implements Job {
 
+    private Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Europe/Minsk"));
+
+    /*
+        Temporary container for holidays.
+     */
+    private HashMap<Integer, List<Integer>> holidays = new HashMap<Integer, List<Integer>>(){{
+        put(11, Arrays.asList(7));
+        put(12, Arrays.asList(25));
+        put(1, Arrays.asList(1, 7));
+        put(3, Arrays.asList(8));
+        put(4, Arrays.asList(8,17));
+        put(5, Arrays.asList(1, 9));
+        put(7, Arrays.asList(3));
+    }};
+
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
         try {
             RepositoryChecker.checkForCommitsInGroups(HibernateShell.getGroupKeeper());
 
-            if(!isSunday()) {
+            if(isMidnight() && !isSunday() && !isHoliday()) {
                 ScheduleChecker.groupScheduleCheck();
             }
 
@@ -29,8 +49,26 @@ public class ComplexCheckTask implements Job {
     }
 
     private boolean isSunday(){
-        Calendar calendar = Calendar.getInstance();
         int day = calendar.get(Calendar.DAY_OF_WEEK);
         return (day == 7);
+    }
+
+    private boolean isHoliday(){
+        int month = calendar.get(Calendar.MONTH) + 1;
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        if(holidays.containsKey(month)){
+            if(holidays.get(month).contains(day))
+                return true;
+        }
+        return false;
+    }
+
+    private boolean isMidnight(){
+        int hour = calendar.get(Calendar.HOUR);
+        int minute = calendar.get(Calendar.MINUTE);
+        int second = calendar.get(Calendar.SECOND);
+
+        return (hour == 0 && minute == 0 && second == 0);
     }
 }
