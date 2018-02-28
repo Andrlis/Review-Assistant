abd0fa55fd62d47f4901d8acdbd8bf4ac57660ef
KessoPavel <pavel.kesso@gmail.com>
2018-02-28 11:48:24 +0300

    Added test Group-Labs-CommentsHibernateShell

---
+++ src/test/java/resources/Hibernate/CommentsHibernateShellTest.java
@@ -0,0 +1,43 @@
+package resources.Hibernate;
+
+import data.Student;
+import data.UniversityClass;
+import data.сomment.Comment;
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+public class CommentsHibernateShellTest {
+
+    @Test
+    public void testComment() {
+        Student student = new Student();
+        student.setFulName("test");
+        student.seteMail("");
+        student.setGitRepoName("");
+        student.setGitURL("");
+
+        UniversityClass universityClass = new UniversityClass();
+        universityClass.setDate(new Date());
+
+        HibernateCore.getInstance().save(student);
+        HibernateCore.getInstance().save(universityClass);
+
+        Comment comment = new Comment();
+        comment.setUniversityClass(universityClass);
+        comment.setStudent(student);
+        comment.setComment("test");
+
+        CommentsHibernateShell commentsHibernateShell = new CommentsHibernateShell();
+
+        commentsHibernateShell.saveComment(comment);
+
+        commentsHibernateShell.deleteComment(comment);
+
+
+        HibernateCore.getInstance().delete(student);
+        HibernateCore.getInstance().delete(universityClass);
+    }
+}
