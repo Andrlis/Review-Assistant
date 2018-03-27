578d6baa7eec3d49f5559605c915985be7da8ce5
Andrey Lisovskiy <andrlis0506@gmail.com>
2018-03-27 23:33:01 +0300

    Add json converter for labs.

---
+++ src/main/java/resources/TableMaker/Convetrters/LabConverter.java
@@ -0,0 +1,21 @@
+package resources.TableMaker.Convetrters;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import data.lab.Lab;
+
+import java.lang.reflect.Type;
+
+public class LabConverter implements JsonSerializer<Lab> {
+    @Override
+    public JsonElement serialize(Lab src, Type typeOfSrc, JsonSerializationContext context) {
+        JsonObject object = new JsonObject();
+
+        object.addProperty("number-of-lab", src.getNumberOfLab());
+        object.addProperty("key-word", src.getKeyWord());
+
+        return object;
+    }
+}
