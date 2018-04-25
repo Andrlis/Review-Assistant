9b802e4e603c0745f58940eaddeda784f076fde6
Julia Runova <jul.runova@gmail.com>
2018-04-25 13:42:28 +0300

    DB structure

--- src/main/java/servlets/UploadStudentInfoFileServlet.java
+++ src/main/java/servlets/UploadStudentInfoFileServlet.java
@@ -7,27 +7,46 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import resources.StudInfoParser;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.IOException;
+import javax.servlet.http.Part;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
+
+@MultipartConfig
 @WebServlet("/UploadStudentInfoFileServlet")
 public class UploadStudentInfoFileServlet extends HttpServlet {
+
+    private static String getValue(InputStream inputStream) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream),1);
+        StringBuilder  value  = new StringBuilder();
+        char[]         buffer = new char[1024];
+        for (int length = 0; (length = reader.read(buffer)) > 0;) {
+            value.append(buffer, 0, length);
+        }
+        return value.toString();
+    }
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //String description = getValue(("description")); // Retrieves <input type="text" name="description">
+        String t = (String)request.getParameter("lecturer");
         if(ServletFileUpload.isMultipartContent(request)){
             try {
-
-
                 List<FileItem> multiparts = new ServletFileUpload(
                         new DiskFileItemFactory()).parseRequest(request);
 
                 for(FileItem item : multiparts){
-                    if(!item.isFormField()){
+                    if(item.getFieldName().equals("teacher")){
+                        item.getName();
+                    } else if(!item.isFormField()){
+
                         File uploadedFile = new File(item.getName());
                         item.write(uploadedFile);
 
@@ -36,10 +55,9 @@ public class UploadStudentInfoFileServlet extends HttpServlet {
                     }
                 }
 
-                //File uploaded successfully
-                request.setAttribute("message", "File Uploaded Successfully");
+
             } catch (Exception ex) {
-                request.setAttribute("message", "File Upload Failed due to " + ex);
+                ex.printStackTrace();
             }
 
         }else{
---
+++ src/main/webapp/js/admin-page.js
@@ -0,0 +1,12 @@
+function showPopupEditGroup() {
+    //$("#id-of-edited-group").val(number);
+    $("#popup-edit-group").modal('show');
+}
+
+
+function showPopupEditSupgroup() {
+    //$("#id-of-edited-subgroup").val($(this).val());
+    $("#popup-edit-subgroup").modal('show');
+}
+
+
